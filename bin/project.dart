67e0a4544ffda1ca5740ee9cf417e6db00e7df02
pc <salenik04@mail.ru>
2023-09-30 11:05:36 +0500

    sucsses

---
+++ bin/project.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+import 'package:dio/dio.dart';
+import 'package:project/project.dart' as project;
+import 'package:test/test.dart';
+
+void main() async {
+  Dio client = Dio();
+  var response = await client.get("https://dummyjson.com/products");
+  Map<String, dynamic> data = response.data;
+  List<dynamic> products = data["products"];
+  List<int> stocks = [];
+  for (dynamic i in products) {
+    int stock = i["stock"];
+    stocks.add(stock);
+  }
+  print(sum(stocks));
+}
+
+int sum(List<int> L) {
+  int result = 0;
+  for (int i in stocks) {
+    result += i;
+  }
+  return result;
+}
